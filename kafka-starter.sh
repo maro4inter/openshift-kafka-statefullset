8fa8fd3a96d33db5de189633e51c5650d84998f5
ARFAOUI Marouene <arfaoui.marouene.devops@gmail.com>
2020-11-05 18:23:10 +0000

    Update kafka-starter.sh

--- kafka-starter.sh
+++ kafka-starter.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ### example kafka-ss-0 will give an id of 0 to the kafka broker.id in the stateful cluster in openshift
 brokerid=$(hostname | awk -F"-" '{print $3}')
-sed -i.bak 's/broker.id=0/broker.id=$brokerid/g' /opt/kafka/config/server.properties
+sed -i.bak 's/broker.id=0/broker.id=$brokerid/g' config/server.properties
 bin/kafka-server-start.sh
