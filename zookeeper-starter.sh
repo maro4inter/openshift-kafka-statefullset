0bf6edddd6d3fb8fe2dfb82c0fa1a48f65e1bf1b
ARFAOUI Marouene <arfaoui.marouene.devops@gmail.com>
2020-11-05 17:06:56 +0000

    Update zookeeper-starter.sh

--- zookeeper-starter.sh
+++ zookeeper-starter.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 ### example zookeeper-ss-0 will give an id of 0 to the zookeeper node in the stateful cluster in openshift
 myid=$(hostname | awk -F"-" '{print $3}')
 echo "$myid" > /data/zookeeper/myid
